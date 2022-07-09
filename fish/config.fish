66da709281b331c1a1249b44cf360d3c44caa2fd
Mark Oborne <markoborne1@gmail.com>
2022-07-09 17:47:05 +0100

    updated prompt

--- fish/config.fish
+++ fish/config.fish
@@ -75,10 +75,10 @@ function fish_mode_prompt; end
 function fish_prompt
     set branch (git branch 2>/dev/null | awk -F '[ ()]'\
         '/*/ { if ($3) print "| "$3" "$6; if (!$3) print "| "$2 }')
-    printf '%s | %s %s\n%s%s%s$ ' (set_color yellow)(hostname) \
+    printf '%s | %s %s\n%s%s$ ' (set_color yellow)(hostname) \
     (set_color bryellow)(prompt_pwd -d 3 -D 2) \
     (set_color yellow)$branch \
-    (jobs | awk 'NR==1{ print $1 }') (set_color bryellow)
+    (jobs | awk 'NR==1{ print "\n"$1 }')(set_color bryellow)
 end
 
 function fix_vwap

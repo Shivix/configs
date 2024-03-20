f32a6f8a5782c3d793b222614ea597bf7b65a3b9
Mark Oborne <markoborne1@gmail.com>
2024-03-20 20:39:50 +0900

    updated

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,31 +1,31 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local on_attach = function(client, _)
+local on_init = function(client, _)
     client.server_capabilities.semanticTokensProvider = nil
 end
 
 require("lspconfig").clangd.setup {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_init = on_init,
 }
 require("lspconfig").pyright.setup {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_init = on_init,
 }
 require("lspconfig").rust_analyzer.setup {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_init = on_init,
 }
 require("lspconfig").gopls.setup {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_init = on_init,
 }
 require("lspconfig").lua_ls.setup {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_init = on_init,
     settings = {
         Lua = {
             runtime = {
-                version = "LuaJIT",
+                version = "Lua 5.4",
             },
             diagnostics = { globals = { "vim" } },
             workspace = {

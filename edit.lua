7bf56d61095d8f149575f1c2b8e03d59b10312cb
Mark Oborne <markoborne1@gmail.com>
2026-03-17 22:23:51 +0900

    feat(edit): allow fuzzy matching configs

--- edit.lua
+++ edit.lua
@@ -5,18 +5,20 @@ end
 
 local configs = dofile("configs.lua")
 
-local pos = assert(input:find("/"), "all files are stored in directories, but non / found in input")
-local name = input:sub(1, pos - 1)
-local file = input:sub(pos + 1)
+local find_match = assert(io.popen("fd . --full-path --type file --hidden configs | fp -e '" .. input .. "'"))
+-- If multiple matches, just get top match.
+local match = find_match:read("*l")
+
+local pos1 = assert(match:find("/"))
+local pos2 = assert(match:find("/", pos1 + 1))
+local name = match:sub(pos1 + 1, pos2 - 1)
+local file = match:sub(pos2 + 1)
 
 local config = configs[name]
 if config == nil then
     error("could not find matching config for: " .. name)
 end
 
-local repo_path = "configs/" .. input
+local repo_path = match
 local system_path = config.path .. "/" .. file
-if not os.execute(string.format("ls %q", repo_path)) then
-    error("could not find file: " .. input)
-end
 os.execute(string.format("nvim -d %s %s", repo_path, system_path))
